733b3db25e0fc24112ac5e0995e0d306edab2c99
phucnguyen06091999 <58318076+phucnguyen06091999@users.noreply.github.com>
2020-04-01 23:53:47 +0700

    git

---
+++ ANDROID_CACULATOR-master/ANDROID_CACULATOR-master/cal1/app/src/main/java/com/example/cal1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cal1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView    textView;
+
+
+    }
+
